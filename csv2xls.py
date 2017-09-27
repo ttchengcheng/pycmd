881dceb0b004cfe9806e1f56df00d00b76f77c6d
Tao Cheng <tchengim30@sina.com>
2017-09-27 09:40:15 +0800

    add `if __name__ == "__main__":

--- csv2xls.py
+++ csv2xls.py
@@ -44,33 +44,33 @@ def save_as_xls(csv_file_path):
 
     return xls_file_path, ""
 
+if __name__ == "__main__":
+    CMD = tc_cmd.INSTANCE
 
-CMD = tc_cmd.INSTANCE
+    # command line arguments
+    PARSER = argparse.ArgumentParser(
+        description='Convert csv file to xls file',
+    )
+    PARSER.add_argument('file', action="store", type=str,
+                        help="input file")
 
-# command line arguments
-PARSER = argparse.ArgumentParser(
-    description='Convert csv file to xls file',
-)
-PARSER.add_argument('file', action="store", type=str,
-                    help="input file")
-
-PARSER.add_argument('--o', action="store_true", dest="open",
-                    help="open xls when converting finished")
+    PARSER.add_argument('--o', action="store_true", dest="open",
+                        help="open xls when converting finished")
 
 
-OPTIONS = PARSER.parse_args()
-FULL_PATH = get_full_path(OPTIONS)
+    OPTIONS = PARSER.parse_args()
+    FULL_PATH = get_full_path(OPTIONS)
 
-if not FULL_PATH:
-    CMD.show_error("File [{0}] cannot be located.".format(OPTIONS.file))
-    sys.exit(2)
+    if not FULL_PATH:
+        CMD.show_error("File [{0}] cannot be located.".format(OPTIONS.file))
+        sys.exit(2)
 
-XLS_FILE_PATH, ERR_MSG = save_as_xls(FULL_PATH)
-if ERR_MSG:
-    CMD.show_error('Saving xls file failed: ' + ERR_MSG)
-    sys.exit(2)
-else:
-    CMD.show_output('File {0} is sucessfully saved'.format(XLS_FILE_PATH))
+    XLS_FILE_PATH, ERR_MSG = save_as_xls(FULL_PATH)
+    if ERR_MSG:
+        CMD.show_error('Saving xls file failed: ' + ERR_MSG)
+        sys.exit(2)
+    else:
+        CMD.show_output('File {0} is sucessfully saved'.format(XLS_FILE_PATH))
 
-if OPTIONS.open:
-    CMD.exec_sys(['open', shlex.quote(XLS_FILE_PATH)])
+    if OPTIONS.open:
+        CMD.exec_sys(['open', shlex.quote(XLS_FILE_PATH)])
